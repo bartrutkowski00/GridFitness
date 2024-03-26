ebfb013720c7e15bb248f951e66b982517aa11ed
bartrutkowski00 <bartlomiej.rutkowski00@gmail.com>
2024-03-26 02:02:53 +0100

    'minor changes'

--- src/app/add-workout/add-workout.component.ts
+++ src/app/add-workout/add-workout.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { WorkoutService } from '../shared/workout.service';
+import { workoutBlueprint } from '../shared/workoutModal';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-workout',
   templateUrl: './add-workout.component.html',
-  styleUrls: ['./add-workout.component.css']
+  styleUrls: ['./add-workout.component.css'],
 })
-export class AddWorkoutComponent {
+export class AddWorkoutComponent implements OnInit, OnDestroy {
+  constructor(private workoutService: WorkoutService) {}
+  workouts: workoutBlueprint[] = [];
+  workoutsSub!: Subscription;
 
+  test() {
+    console.log(this.workouts);
+  }
+
+  ngOnInit() {
+    this.workoutService.getWorkoutsFromDatabase();
+    this.workoutsSub = this.workoutService.workoutsChange.subscribe(
+      (workoutsArr) => {
+        this.workouts = workoutsArr;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {}
 }
